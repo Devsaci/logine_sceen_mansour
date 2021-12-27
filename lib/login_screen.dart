4a454ae65c76681b172b30a4342c3d3688c3986e
saci zakaria <sacideveloppeur@gmail.com>
2021-12-27 19:40:14 +0100

    Login Screen Mansour

---
+++ lib/login_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LOGINE SSCREEN'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("data")],
+        ),
+      ),
+    );
+  }
+}
