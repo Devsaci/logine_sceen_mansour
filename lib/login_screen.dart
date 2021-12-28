e4aad0ba3f7244b570fe8519b8750fa7763cd87c
saci zakaria <sacideveloppeur@gmail.com>
2021-12-28 16:47:25 +0100

    hintText

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -24,7 +24,7 @@ class LoginScreen extends StatelessWidget {
             ),
             TextFormField(
               decoration: const InputDecoration(
-
+                  hintText: 'LOGIN',
               ),
             ),
 
