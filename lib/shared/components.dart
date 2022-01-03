b9013a0096e814cfebe47b7adfa4453c993e6947
saci zakaria <sacideveloppeur@gmail.com>
2022-01-03 16:28:46 +0100

    Container

--- lib/shared/components.dart
+++ lib/shared/components.dart
@@ -1,3 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-Widget? defaultButton() => null;
+Widget? defaultButton() => Container(
+      width: double.infinity,
+      color: Colors.red,
+      child: MaterialButton(
+        child: Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          // print(emailController.text);
+          // print(passwordController.text);
+        },
+      ),
+    );
